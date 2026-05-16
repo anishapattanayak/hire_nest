84c9e932f841f0044fbd9c8ac757c56b8010c65d
Anishapattanayak <anishapattanayak74@gmail.com>
2026-05-16 13:03:03 +0530

    updated file constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"; 
-export const NOTIFICATION_API_END_POINT = "http://localhost:8000/api/v1/notification";
+export const USER_API_END_POINT="https://hire-nest-fcx5.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://hire-nest-fcx5.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://hire-nest-fcx5.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://hire-nest-fcx5.onrender.com/api/v1/company"; 
+export const NOTIFICATION_API_END_POINT = "https://hire-nest-fcx5.onrender.com/api/v1/notification";
 
 
